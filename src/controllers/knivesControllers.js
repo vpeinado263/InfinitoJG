78cc461901b1ccac450c10f04671775280b949fb
Victor Peinado <vpeinado263@gmail.com>
2024-02-10 11:41:00 -0300

    Se realizaron cambios en la logica e los controladores

--- src/controllers/knivesControllers.js
+++ src/controllers/knivesControllers.js
@@ -1,6 +1,62 @@
 const knifeModel = require('../models/knifeModel');
 const { validationResult } = require('express-validator');
 
+//crear cuchillos
+const createKnife = async (req, res) => {
+  try {
+      const newKnife = await knifeModel.create(req.body);
+      res.status(201).json({ knife: newKnife, msg: "Cuchillo creado" });
+  } catch (error) {
+      res.status(500).json({
+          knife: null,
+          msg: "Error al crear el cuchillo - " + error.message,
+      });
+  }
+};
+
+//ver todos los cuchillos creados
+const getKnives = async (req, res) => {
+  try {
+    const knives = await knifeModel.find();
+    res.status(200).json({ knives, msg: "Todos los cuchillos de InfinitoJG" });
+  } catch (error) {
+    res.status(500).json({ 
+        knives: null,
+        msg: "Error al obtener cuchillos - " + error.message, 
+    });
+  }
+};
+
+//obtenr segun ID
+const getKnivesById = async (req, res) => {
+  try {
+      const knife = await knifeModel.findById(req.params.id);
+      res.status(200).json({ knife, msg: "ok" });
+  } catch (error) {
+      res.status(500).json({
+          knife: null,
+          msg: "Error al obtener cuchillo - " + error.message,
+      });
+  }
+};
+
+
+//obtenr segun CODIGO
+const getKnivesByCode = async (req, res) => {
+  try {
+      const knife = await knifeModel.findOne({ codigo: req.params.codigo });
+      if (!knife) {
+          return res.status(404).json({ msg: 'Cuchillo no encontrado' });
+      }
+      res.status(200).json({ knife, msg: 'ok' });
+  } catch (error) {
+      res.status(500).json({
+          knife: null,
+          msg: 'Error al obtener cuchillo - ' + error.message,
+      });
+  }
+};
+
 // actualizar un cuchillo por su ID
 const updateKnifeByID = async (req, res) => {
     try {
@@ -42,58 +98,6 @@ const updateKnifeByID = async (req, res) => {
     }
   };
   
-//ver todos los cuchillos creados
-const getKnives = async (req, res) => {
-  try {
-    const knives = await knifeModel.find();
-    res.status(200).json({ knives, msg: "Todos los cuchillos Gamer Master Knives" });
-  } catch (error) {
-    res.status(500).json({ 
-        knives: null,
-        msg: "Error al obtener cuchillos - " + error.message, 
-    });
-  }
-};
-//obtenr segun ID
-const getKnivesById = async (req, res) => {
-    try {
-        const knife = await knifeModel.findById(req.params.id);
-        res.status(200).json({ knife, msg: "ok" });
-    } catch (error) {
-        res.status(500).json({
-            knife: null,
-            msg: "Error al obtener cuchillo - " + error.message,
-        });
-    }
-};
-//obtenr segun CODIGO
-const getKnivesByCode = async (req, res) => {
-  try {
-      const knife = await knifeModel.findOne({ codigo: req.params.codigo });
-      if (!knife) {
-          return res.status(404).json({ msg: 'Cuchillo no encontrado' });
-      }
-      res.status(200).json({ knife, msg: 'ok' });
-  } catch (error) {
-      res.status(500).json({
-          knife: null,
-          msg: 'Error al obtener cuchillo - ' + error.message,
-      });
-  }
-};
-//crear cuchillos
-const createKnife = async (req, res) => {
-    try {
-        const newKnife = await knifeModel.create(req.body);
-        res.status(201).json({ knife: newKnife, msg: "Cuchillo creado" });
-    } catch (error) {
-        res.status(500).json({
-            knife: null,
-            msg: "Error al crear el cuchillo - " + error.message,
-        });
-    }
-};
-
 module.exports = { 
     getKnives, 
     getKnivesById, 

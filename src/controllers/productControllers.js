320a84c03406b3607fcdf1cf9f3fa28458af4a67
Victor Peinado <vpeinado263@gmail.com>
2024-02-11 12:29:09 -0300

    controllers de API de Platzi Fake Store

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -1,15 +1,16 @@
 const axios = require('axios');
 
 // API de Platzi Fake Store
+
 const createProduct = async (res) => {
   try {
     const response = await axios.post('https://api.escuelajs.co/api/v1/products');
     const newProducts = response.data;
-    res.status(201).json({ products: newProducts, msg: 'Producto obtenido correctamente.' });
+    res.status(201).json({ products: newProducts, msg: '¡Producto Obtenido Correctamente.!' });
   } catch (error) {
     res.status(500).json({
       products: null,
-      msg: "Error al crear el producto - " + error.message,
+      msg: "Error al Crear el Producto - " + error.message,
     });
   }
 };
@@ -18,7 +19,7 @@ const getAllProducts = async (res) => {
   try {
     const response = await axios.get('https://api.escuelajs.co/api/v1/products');
     const products = response.data;
-    res.status(200).json({ products, msg: "Todos los productos de Platzi Fake Store API." });
+    res.status(200).json({ products, msg: "¡Todos los productos de Platzi Fake Store API.!" });
   } catch (error) {
     res.status(500).json({
       products: null,
@@ -32,7 +33,7 @@ const getProductById = async (req, res) => {
     const { id } = req.params;
     const response = await axios.get(`https://api.escuelajs.co/api/v1/products/${id}`);
     const product = response.data;
-    res.status(200).json({ product, msg: 'Producto obtenido correctamente.' });
+    res.status(200).json({ product, msg: '¡Producto obtenido correctamente.!' });
   } catch (error) {
     res.status(500).json({ product: null, msg: 'Error al obtener el producto - ' + error.message });
   }
@@ -43,7 +44,7 @@ const updateProduct = async (req, res) => {
     const { id } = req.params;
     const response = await axios.put(`https://api.escuelajs.co/api/v1/products/${id}`, req.body);
     const updatedProduct = response.data;
-    res.status(200).json({ product: updatedProduct, msg: 'Producto actualizado correctamente.' });
+    res.status(200).json({ product: updatedProduct, msg: '¡Producto actualizado correctamente.!' });
   } catch (error) {
     res.status(500).json({ msg: 'Error al actualizar el producto - ' + error.message });
   }
@@ -55,7 +56,7 @@ const deleteProduct = async (req, res) => {
     const response = await axios.delete(`https://api.escuelajs.co/api/v1/products/${id}`);
     const success = response.data;
     if (success) {
-      res.status(200).json({ msg: 'Producto eliminado correctamente.' });
+      res.status(200).json({ msg: '¡Producto eliminado correctamente.!' });
     } else {
       res.status(404).json({ msg: 'Producto no encontrado.' });
     }

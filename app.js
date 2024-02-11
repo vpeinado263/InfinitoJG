bc9544b15d5f2d1151fa093b761567c4a0000e27
Victor Peinado <vpeinado263@gmail.com>
2024-02-11 01:20:12 -0300

    se realizan cambio a infinitoJG

--- app.js
+++ app.js
@@ -2,24 +2,17 @@ const express = require('express');
 const app = express();
 const logRequest = require('../Proyecto-Final-Backend/src/middlewares/logRequest');
 const knivesRouter = require('../Proyecto-Final-Backend/src/routes/knivesRoutes');
-const productControllers = require('../Proyecto-Final-Backend/src/controllers/productControllers');
+const productsRouter = require('../Proyecto-Final-Backend/src/routes/productRoutes')
 
 
 app.use(express.json());
 app.use(logRequest);
 
 app.use('/knives', knivesRouter);
-
-app.use('/products', productControllers.getAllProducts);
-app.get('/products/:id', productControllers.getProductById);
-app.post('/products', productControllers.createProduct);
-app.put('/products/:id', productControllers.updateProduct);
-app.delete('/products/:id', productControllers.deleteProduct);
+app.use('/products', productsRouter)
 
 app.use('/', (req, res) => {
   res.send('Bienvenidos a InfinitoJG!');
 });
-
-
 
 module.exports = app;

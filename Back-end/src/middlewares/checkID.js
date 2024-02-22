6c435efe0d5e71742a08b5f930d3ef715b3b0d18
Victor Peinado <vpeinado263@gmail.com>
2024-02-22 10:19:44 -0300

    cambios en la estructuracion y separacion

--- Back-end/src/middlewares/checkID.js
+++
@@ -1,9 +0,0 @@
-const checkID = (req, res, next) => {
-    const id = req.params.id;
-    if (!id || !id.match(/^[0-9a-fA-F]{24}$/)) {
-      return res.status(400).json({ msg: 'ID inválido' });
-    }
-    next();
-  };
-  
-  module.exports = checkID;
